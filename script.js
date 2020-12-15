4e90d2be29d65c77507763d3d436958bac75e414
luis alberto J.S <luis.albertosax@gmail.com>
2020-12-15 20:39:10 -0300

    Output

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 alert("Hello World");
+
+document.getElementById("titulo").innerHTML = "Oi, tudo bem!";
+document.write("Vamos desenvolver!");
+
+window.alert("E aí vamos programar!");
+
+console.log("Mensagem para o console.");
